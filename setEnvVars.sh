b2ee253a1e90ca44610f7e93160c958ed9711c6b
Pawas Aggarwal <paaggarwal@dminc.com>
2023-02-09 14:36:54 -0500

    test

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
-export GITHUB_USERNAME="<<username>>"
-export GITHUB_REPO="bootcamp-2022-sls-api-<<lastname>>"
-export BUCKET_NAME="<<username>>-bootcamp-2022-bucket" #username must be all lowercase
+export GITHUB_USERNAME="TheSpeedForce"
+export GITHUB_REPO="bootcamp-2022-sls-api-aggarwal"
+export BUCKET_NAME="pawas-bootcamp-2022-bucket" #username must be all lowercase
